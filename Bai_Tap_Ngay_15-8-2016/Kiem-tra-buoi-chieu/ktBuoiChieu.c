1edc7e3de7a015d21c04de3f671a35a0dd8d6eef
NguyenHoangPhuTien <tien.phu.nguyen01@gmail.com>
2016-08-15 17:04:12 +0700

    Bai tap ngay 15/8/2016
    - Check afternoon in day with char[].
    - Check afternoon in day with char.
    - File execute.

---
+++ Bai_Tap_Ngay_15-8-2016/Kiem-tra-buoi-chieu/ktBuoiChieu.c
@@ -0,0 +1,16 @@
+void main(){
+
+        char tl;
+        printf("Dang la buoi chieu phai khong(y/n)?: ");
+        scanf("%c", &tl);
+        if(tl == 'y'|| tl == 'Y')
+        {
+                printf("Xin chao buoi chieu\n");
+        }
+	else
+	{
+		printf("khong phai buoi chieu\n");
+	}
+	exit(0);
+}
+
---
+++ Bai_Tap_Ngay_15-8-2016/Kiem-tra-buoi-chieu/ktBuoiChieu_2.c
@@ -0,0 +1,14 @@
+void main(){
+
+	char ans[50];
+	printf("day co la buoi chieu ?: ");
+	scanf("%s", &ans);
+	if(strcmp(ans,"yes") == 0)
+	{
+		printf("day la buoi chieu");
+	}
+	else
+	{
+		printf("ko phai la buoi chieu");
+	}
+}
